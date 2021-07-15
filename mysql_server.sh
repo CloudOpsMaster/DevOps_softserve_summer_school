45ceb21d5279823859a2819d6852353f6fcee149
Maks0123 <vadimcorporateukr@gmail.com>
2021-07-15 12:25:28 +0300

    fix bind ip

--- mysql_server.sh
+++ mysql_server.sh
@@ -39,9 +39,9 @@ sudo mysql -u root -p
 echo "finished setting up...................................................... "
 
 
-echo "Updating mysql configs in /etc/mysql/my.cnf."
-sudo sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/my.cnf
-echo "Updated mysql bind address in /etc/mysql/my.cnf to 0.0.0.0 to allow external connections."
+echo "Updating mysql configs in /etc/mysql/mysql.conf.d/mysqld.cnf"
+sudo sed -i "s/.*bind-address.*/bind-address = 0.0.0.0/" /etc/mysql/mysql.conf.d/mysqld.cnf
+echo "Updated mysql bind address in /etc/mysql/mysql.conf.d/mysqld.cnf to 0.0.0.0 to allow external connections."
 
 echo "Stop mysql"
 sudo service mysql stop
