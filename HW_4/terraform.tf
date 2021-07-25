7bdd18982af37208c993dcb531ff1f679b445083
Maks0123 <vadimcorporateukr@gmail.com>
2021-07-25 20:31:26 +0300

    22 port for ssh

--- HW_4/terraform.tf
+++ HW_4/terraform.tf
@@ -41,6 +41,15 @@ resource "aws_security_group" "webserver" {
     ipv6_cidr_blocks = ["::/0"]
   }
 
+    ingress {
+    description      = "tcp port 22"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
