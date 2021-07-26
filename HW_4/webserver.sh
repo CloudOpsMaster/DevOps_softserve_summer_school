b441c4ba9a946f05e9cc462a42ef116a34d12187
Maks0123 <vadimcorporateukr@gmail.com>
2021-07-26 20:04:27 +0300

    change ssh script

--- HW_4/webserver.sh
+++ HW_4/webserver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
-sudo apt install httpd
+sudo apt install apache2
 myip='curl http://169.254.169.254/latest/meta-data/local-ipv4'
 echo "<h2>WebServer with IP: $myip</h2><br>Build by Terraform for SoftServe!" > /var/www/html/index.html
-sudo service httpd start
-chkconfig httpd on
+sudo service apache2 start
+sudo service apache2 status
